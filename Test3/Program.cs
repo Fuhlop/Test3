e29de828ed1e526194de12ac84c40d3277932d7e
Phillip Youssef <Phillip Youssef@DESKTOP-9ENO0TT>
2020-12-15 21:40:36 -0600

    Add project files.

---
+++ Test3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jah = 3;
+            int x = 0;
+            if (x == 1)
+            {
+                Console.WriteLine("111");
+            }
+            if (x == 2)
+            {
+                while (jah >= 0)
+                {
+                    Console.WriteLine("111");
+                    jah--;
+                }
+            } else
+            {
+                Console.WriteLine("222");
+
+            }
+           //not sure why the jah-- isnt working 
+           //i dont know the symbol for (not true) and for or statements
+           //
+         
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
